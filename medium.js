9520f6efe9a64896a89d5066849d2d8ce443fcb8
marcosocon <oconmarcos97@gmail.com>
2016-04-01 18:51:12 -0300

    added accounts, delete post buttons

--- medium.js
+++ medium.js
@@ -1,13 +1,13 @@
+//DB collection.
 Posts = new Mongo.Collection("posts");
 if (Meteor.isClient) {
-
-
+//BODY - helpers.
   Template.body.helpers({
     posts:function(){
       return Posts.find();
     }
   });
-
+//POSTFORM - events.
   Template.postform.events({
     "submit .post-form": function (event) {
     var title = event.target.title.value;
@@ -22,19 +22,30 @@ if (Meteor.isClient) {
         Posts.insert({
           title:title,
           body:body,
+          owner: Meteor.userId(),
+          author:Meteor.user().emails[0].address,
           tag:tag
         });
 
         event.target.title.value = '';
         event.target.body.value = '';
         event.target.tag.value = '';
-
         return false
       }
     }
   });
-
-
+//POST - helpers.
+  Template.post.helpers({
+    isOwner:function(){
+      return this.owner === Meteor.userId();
+    }
+  });
+//POST - events.
+  Template.post.events({
+    "click .remove-post":function(){
+      Posts.remove(this._id)
+    }
+  });
 }
 
 if (Meteor.isServer) {
